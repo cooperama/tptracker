d31e8bc38a1fa2ad4dde44b107dcd08c9f296f21
bhowe111 <brendan.howe1@gmail.com>
2020-11-23 11:53:27 -0800

    Added dependencies and landing page

---
+++ tp-api/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+const PORT = process.env.PORT || 4000;
+const app = express();
+
+const corsOptions = {origin: 'http://localhost:3000'};
+
+// -------------------------- MIDDLEWARE
+app.use(express.json())
+app.use(cors(corsOptions))
+
+// -------------------------- MIDDLEWARE API
+app.use("/api/v1/products", routes.products);
+app.use("/api/v1/stores", routes.stores);
+
+
+// -------------------------- CONNECTION
+app.listen(port, () => console.log(`Server running on PORT: ${port}`));
