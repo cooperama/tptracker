a144962eba8e3f8539a6d552f956319e67c8a2d8
cooperama <coope133@gmail.com>
2020-11-23 17:43:37 -0800

    Exports routes from routes

--- tp-client/src/App.js
+++ tp-client/src/App.js
@@ -1,19 +1,19 @@
 import React from "react";
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
-import routes from "./config/routes";
 import NavBar from "./components/Navbar";
 import ProductList from "./pages/ProductList";
+import StoreList from "./pages/StoreList";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      {/* {routes} */}
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/products" component={ProductList} />
+        <Route exact path="/stores" component={StoreList} />
       </Switch>
     </div>
   );
