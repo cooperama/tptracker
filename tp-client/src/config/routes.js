392a905a6a1effb36850dd75c4d45dbfd0340d47
emforest <em.forester426@gmail.com>
2020-11-23 21:54:31 -0800

    started product page css

--- tp-client/src/config/routes.js
+++ tp-client/src/config/routes.js
@@ -4,14 +4,10 @@ import { Switch, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import ProductList from "../pages/ProductList";
 import StoreList from "../pages/StoreList";
-<<<<<<< HEAD
-import Cart from '../pages/Cart';
-import Checkout from '../pages/Checkout';
-=======
+
 import Cart from "../pages/Cart";
 import ProductShow from "../pages/ProductShow";
 import StoreShow from "../pages/StoreShow";
->>>>>>> ebb668dc24952e383afa2fce89c1bba8701a7848
 
 // const routes = () => {
 //   <Switch>
@@ -26,12 +22,8 @@ export default (
     <Route exact path="/stores" component={StoreList} />
     <Route exact path="/products" component={ProductList} />
     <Route path="/cart" component={Cart} />
-<<<<<<< HEAD
-    <Route path="/checkout" component={Checkout} />
-=======
     <Route path="/products/:productId" component={ProductShow} />
     <Route path="/stores/:storeId" component={StoreShow} />
->>>>>>> ebb668dc24952e383afa2fce89c1bba8701a7848
   </Switch>
 );
 
