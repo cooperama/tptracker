d1f8af02363bb5ed7d28ac8572b20691b336f0ed
cooperama <coope133@gmail.com>
2020-11-24 11:38:00 -0800

    Adds styles

--- tp-client/src/components/Navbar.js
+++ tp-client/src/components/Navbar.js
@@ -4,14 +4,6 @@ import { Link } from "react-router-dom";
 function NavBar(props) {
   return (
     <nav>
-      {/* <ul>
-        <li className="navLi">
-          <Link to="/">Home</Link>
-          <Link to="/products">All Products</Link>
-          <Link to="/stores">All Stores</Link>
-          <Link to="/cart">Cart</Link>
-        </li>
-      </ul> */}
       <i className="fas fa-chevron-left"></i>
       <h2 className="brand-name">Essentials.</h2>
       <i className="fas fa-shopping-cart"></i>
--- tp-client/src/pages/Confirmed.js
+++ tp-client/src/pages/Confirmed.js
@@ -3,8 +3,25 @@ import React from "react";
 export default function Confirmed() {
   return (
     <div>
-      <h1>Confirmed</h1>
+      <div className="nav-cover"></div>
       <div className="main-content"></div>
+      <img
+        className="confirmed-image"
+        src="https://www.flaticon.com/svg/static/icons/svg/2997/2997154.svg"
+        alt="confirmed"
+      />
+      <div className="confirmation-text">
+        <h1>Your order has been confirmed!</h1>
+        <p>Please have your confirmation code ready to show at pick-up.</p>
+      </div>
+      <div className="submain-content">
+        <div className="confirmation-code">
+          <p>FG766KJ9</p>
+        </div>
+        <a href="/">
+          <button className="back-home-btn">Back to Home</button>
+        </a>
+      </div>
     </div>
   );
 }
